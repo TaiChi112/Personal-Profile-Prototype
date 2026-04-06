e44fd99c56226eac0926e6950edb40f2774d0631
TaiChi <anothai.0978452316@gmail.com>
2026-04-06 23:03:03 +0700

    chore(prisma): add auth and post migrations

---
+++ prisma/migrations/20260405092652_align_prisma_nextjs_structure/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "public"."Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Post_authorId_idx" ON "public"."Post"("authorId");
+
+-- AddForeignKey
+ALTER TABLE "public"."Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
