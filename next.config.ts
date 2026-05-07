d556bae4da2fe4fc8d3dd87ca450baabaaf7d1d9
TaiChi <anothai.0978452316@gmail.com>
2026-05-08 00:06:46 +0700

    feat: add custom headers configuration to next.js for improved server identification

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,21 @@ const withMDX = createMDX();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        // บังคับให้ใส่ Header นี้ในทุกๆ หน้าเว็บ (/(.*))
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Served-By',
+            // ดึงค่าจาก .env ถ้าหาไม่เจอให้ขึ้นว่า Local-Dev
+            value: process.env.SERVER_NODE || 'Local-Dev',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withMDX(nextConfig);
