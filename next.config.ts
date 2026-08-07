33fbd57f72362987e1db386e4a55d82a305faa30
Anothai Vichapaiboon <anothai.0978452316@gmail.com>
2026-08-07 15:41:43 +0700

    fix: reorder dev scripts in package.json for clarity refactor: update next.config.ts to include experimental package imports

--- next.config.ts
+++ next.config.ts
@@ -4,23 +4,10 @@ import { createMDX } from 'fumadocs-mdx/next';
 const withMDX = createMDX();
 
 const nextConfig: NextConfig = {
-  output: 'standalone'
-  /* config options here */
-  // async headers() {
-  //   return [
-  //     {
-  //       // บังคับให้ใส่ Header นี้ในทุกๆ หน้าเว็บ (/(.*))
-  //       source: '/(.*)',
-  //       headers: [
-  //         {
-  //           key: 'X-Served-By',
-  //           // ดึงค่าจาก .env ถ้าหาไม่เจอให้ขึ้นว่า Local-Dev
-  //           value: process.env.SERVER_NODE || 'Local-Dev',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  output: 'standalone',
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'framer-motion'],
+  },
 };
 
 export default withMDX(nextConfig);
