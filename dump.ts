4712a9ed2f43573b6d258a6e670d0c407143fa97
Anothai Vichapaiboon <anothai.0978452316@gmail.com>
2026-08-29 23:50:55 +0700

    refactor: remove unused dump, proxy, and test_fumadocs files

--- dump.ts
+++
@@ -1,4 +0,0 @@
-import { source } from "./app/lib/source";
-
-const pageEn = source.getPage(undefined, 'en');
-console.log("EN Page:", pageEn ? pageEn.url : null);
--- test_fumadocs.ts
+++
@@ -1,3 +0,0 @@
-import { source } from "./app/lib/source";
-const page = source.getPage(['artificial-intelligence', 'dm', 'Demo'], 'en');
-console.log(page);
