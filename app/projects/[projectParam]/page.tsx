0ff106a3f2281822af92d32962ce7305dc663e1f
Anothai Vichapaiboon <anothai.0978452316@gmail.com>
2026-08-30 23:38:06 +0700

    feat: refactor ProjectDetailPage to use async/await for data fetching; enhance component props with additional data

--- app/projects/[projectParam]/page.tsx
+++ app/projects/[projectParam]/page.tsx
@@ -1,11 +1,20 @@
-"use client";
-
-import { useParams } from 'next/navigation';
 import { PersonalWebsiteApp } from '../../features/composition/PersonalWebsiteApp';
+import { fetchAllKeystaticData } from '../../lib/content-fetcher';
 
-export default function ProjectDetailPage() {
-  const params = useParams<{ projectParam: string }>();
-  const projectParam = Array.isArray(params.projectParam) ? params.projectParam[0] : params.projectParam;
+export default async function ProjectDetailPage({ params }: { params: Promise<{ projectParam: string }> }) {
+  const resolvedParams = await params;
+  const projectParam = Array.isArray(resolvedParams.projectParam) ? resolvedParams.projectParam[0] : resolvedParams.projectParam;
+  
+  const { projectsList, blogsTree, articlesTree, blogsList } = await fetchAllKeystaticData();
 
-  return <PersonalWebsiteApp initialTab="projects" initialProjectParam={projectParam} />;
+  return (
+    <PersonalWebsiteApp 
+      initialTab="projects" 
+      initialProjectParam={projectParam} 
+      initialProjectsList={projectsList}
+      initialBlogsTree={blogsTree}
+      initialArticlesTree={articlesTree}
+      blogsList={blogsList}
+    />
+  );
 }
